449a559790e2b97ba5442106ad5dbbe74bb5ba3b
ahmed najash <ahmednjsh1@gmail.com>
2024-09-05 08:40:01 +0300

    Add Play Fetch solution with bonus options

---
+++ Assets/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float lastSpawn = 0f;
+    public float spawnDelay = 1.5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog after 3 seconds pass
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > lastSpawn + spawnDelay)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastSpawn = Time.time;
+        }
+    }
+}
